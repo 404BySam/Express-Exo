5d3b832e1ab51dd4ac283a5787ae61c811f024a9
404BySam <devbysam404@gmail.com>
2025-09-26 22:34:58 +0200

    test to add controller

--- index.js
+++ index.js
@@ -3,67 +3,8 @@ const app = express();
 
 const port = 5000;
 
-const movies = [
-  {
-    id: 1,
-
-    title: "Citizen Kane",
-
-    director: "Orson Wells",
-
-    year: "1941",
-
-    color: false,
-
-    duration: 120,
-  },
-
-  {
-    id: 2,
-
-    title: "The Godfather",
-
-    director: "Francis Ford Coppola",
-
-    year: "1972",
-
-    color: true,
-
-    duration: 180,
-  },
-
-  {
-    id: 3,
-
-    title: "Pulp Fiction",
-
-    director: "Quentin Tarantino",
-
-    year: "1994",
-
-    color: true,
-
-    duration: 180,
-  },
-];
-
 app.get("/", (req, res) => {
   res.send("Salut Bienvenue sur ma liste favorite!");
-});
-
-app.get("/api/movies", (req, res) => {
-  res.status(200).json(movies);
-});
-
-app.get("/api/movies/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const found = movies.find((element) => element.id === id);
-
-  if (!found) {
-    res.status(404).send(`Il existe pas de film avec cet ID: ${id}`);
-  } else {
-    res.send(found);
-  }
 });
 
 app
